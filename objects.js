2a695da33667da896da6bc86cdf8fd3a7bda95e8
emiliendeon <emilien.deon@gmail.com>
2021-07-24 16:47:33 +0200

    edit object except function

--- objects.js
+++ objects.js
@@ -8,13 +8,10 @@ const select = (object, keys) => keys.reduce((acc, key) => ({ ...acc, [key]: obj
 const mapAndSelect = (objects, keys) => objects.map((object) => select(object, keys));
 
 // exclude some keys of an object (useful to hide or optimize data)
-const except = (object, keys) => {
-	let newObject = { ...object };
-	for (const key of keys) {
-		delete newObject[key];
-	}
-	return newObject;
-};
+const except = (object, keys) =>
+	Object.keys(object)
+		.filter((key) => !keys.includes(key))
+		.reduce((acc, key) => ({ ...acc, [key]: object[key] }), {});
 
 // apply except operation on an array of objects
 const mapAndExcept = (objects, keys) => objects.map((object) => except(object, keys));
